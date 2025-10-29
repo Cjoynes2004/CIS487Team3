f456043344d12ea7ce31432ba182d7c7b8df2811
Chandler <cjoynes@umich.edu>
2025-10-29 14:34:42 -0400

    Added player animations

--- IntoTheTower/Assets/C# Scripts/Player Scripts/MovePlayer.cs
+++ IntoTheTower/Assets/C# Scripts/Player Scripts/MovePlayer.cs
@@ -5,10 +5,13 @@ public class MovePlayer : MonoBehaviour
 {
     public float playerSpeed; // Speed of the player
     public bool canMove = true; // Determines whether player can move or shoot
+
+
+    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,10 +30,48 @@ public class MovePlayer : MonoBehaviour
 
             if (hInput != 0 || vInput != 0)
             {
-                Vector3 moveDirection;
+                animator.SetBool("isWalking", true);
+                if (hInput > 0 && vInput == 0)
+                {
+                    animator.SetInteger("faceDirection", 3);
+                }
+                else if (hInput < 0 && vInput == 0)
+                {
+                    animator.SetInteger("faceDirection", 7);
+                }
+                else if (hInput == 0 && vInput < 0)
+                {
+                    animator.SetInteger("faceDirection", 1);
+                }
+                else if (hInput == 0 && vInput > 0)
+                {
+                    animator.SetInteger("faceDirection", 5);
+                }
+                else if (hInput < 0 && vInput > 0)
+                {
+                    animator.SetInteger("faceDirection", 6);
+                }
+                else if (hInput > 0 && vInput < 0)
+                {
+                    animator.SetInteger("faceDirection", 2);
+                }
+                else if (hInput < 0 && vInput < 0)
+                {
+                    animator.SetInteger("faceDirection", 8);
+                }
+                else if (hInput > 0 && vInput > 0)
+                {
+                    animator.SetInteger("faceDirection", 4);
+                }
+                    Vector3 moveDirection;
                 moveDirection = new Vector3(hInput, vInput, 0f);
                 moveDirection.Normalize();
                 transform.Translate(moveDirection * playerSpeed * Time.deltaTime, Space.World);
+            }
+            else
+            {
+                animator.SetBool("isWalking", false);
+                animator.SetInteger("faceDirection", 0);
             }
         }
     }
