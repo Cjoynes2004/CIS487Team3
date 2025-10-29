94413ab5bc95444fec5d022829b927783d6bd7b3
Chandler <cjoynes@umich.edu>
2025-10-28 23:51:19 -0400

    Updated reflection bullet code

--- IntoTheTower/Assets/C# Scripts/Player Scripts/PlayerBullets.cs
+++ IntoTheTower/Assets/C# Scripts/Player Scripts/PlayerBullets.cs
@@ -5,9 +5,12 @@ public class PlayerBullets : MonoBehaviour
 {
     public int chainShotLength;
     public TrapdoorBehavior reflectTrapdoor;
+
     private bool[] chainShot;
     private Rigidbody2D rb;
-    SpriteRenderer sprite;
+    private SpriteRenderer sprite;
+    private bool bounceRegistered = false;
+
 
     private void Start()
     {
@@ -18,6 +21,10 @@ public class PlayerBullets : MonoBehaviour
     //Damages enemy on collisin and erases bullet regardless
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (bounceRegistered) return;
+        bounceRegistered = true;
+        Invoke(nameof(ResetBounce), 0.05f); // short cooldown before next bounce counts
+
         AbstractEnemy enemy = collision.gameObject.GetComponent<AbstractEnemy>();
         ReflectSurface bounce = collision.gameObject.GetComponent<ReflectSurface>();
 
@@ -31,24 +38,24 @@ public class PlayerBullets : MonoBehaviour
         {
             sprite.color = Color.green;
             rb.linearVelocity = Vector2.Reflect(rb.linearVelocity, collision.contacts[0].normal);
-            for (int i = 0; i < chainShot.Length; ++i)
+            for (int i = 0; i <= chainShot.Length; ++i)
             {
                 if (!chainShot[i])
                 {
                     chainShot[i] = true;
-                    break;
-                }
-                if (i == chainShotLength - 1 && chainShot[i])
-                {
-                    for (int y = 0; y < reflectTrapdoor.openFlags.Count; y++)
+                    if (i == chainShot.Length - 1)
                     {
-                        if (!reflectTrapdoor.openFlags[y])
+                        for (int y = 0; y < reflectTrapdoor.openFlags.Count; y++)
                         {
-                            reflectTrapdoor.openFlags[y] = true;
-                            break;
+                            if (!reflectTrapdoor.openFlags[y])
+                            {
+                                reflectTrapdoor.openFlags[y] = true;
+                                break;
+                            }
                         }
                     }
-                }
+                    break;
+                }               
             }
         }
         else
@@ -56,5 +63,10 @@ public class PlayerBullets : MonoBehaviour
             Destroy(gameObject);
             Destroy(this);
         }
+    }
+
+    private void ResetBounce()
+    {
+        bounceRegistered = false;
     }
 }
